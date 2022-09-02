6f2f0e3d1e865e529f5beed4a1ba397e7bfc9d49
Xirvin <103050557+ImXirvin@users.noreply.github.com>
2022-09-02 15:08:55 +1000

    Initial commit

---
+++ ui/src/providers/VisibilityProvider.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { ReceiveNUI } from '../utils/ReceiveNUI';
+  import { SendNUI } from '../utils/SendNUI';
+  import { onMount } from 'svelte';
+  import { visibility } from '../store/stores';
+
+
+  let isVisible: boolean;
+
+  visibility.subscribe((visible) => {
+    isVisible = visible;
+  });
+
+  ReceiveNUI<boolean>('setVisible', (visible) => {
+    visibility.set(visible);
+  });
+
+
+  onMount(() => {
+    const keyHandler = (e: KeyboardEvent) => {
+      if (isVisible && ['Escape'].includes(e.code)) {
+        SendNUI('hideUI');
+        visibility.set(false);
+      }
+    };
+
+    window.addEventListener('keydown', keyHandler);
+  
+
+    return () => window.removeEventListener('keydown', keyHandler);
+  });
+</script>
+
+<main>
+  {#if isVisible}
+    <slot />
+  {/if}
+</main>
+
